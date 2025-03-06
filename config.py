18e15f84602dfd500d49e898c0b669433b88bba2
NOOB-CODER-RAJA <sudheerpal8257@gmail.com>
2025-03-06 23:24:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,8 +56,8 @@ GIT_TOKEN = getenv(
 )  # ----------------------------------------------------------------
 # -------------------------------------------------------------------
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PURVI_SUPPORT")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PURVI_UPDATES")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/kedi_update")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Sprzn_arora_appeal")
 
 # --------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------
